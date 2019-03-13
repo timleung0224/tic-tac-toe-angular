817968acd18b788c97cccc116f183686080c9859
timleung0224 <timleung0224@gmail.com>
2019-03-13 10:29:39 +0800

    add mis files

---
+++ src/app/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-spinner',
+  templateUrl: './loading-spinner.component.html',
+  styleUrls: ['./loading-spinner.component.css']
+})
+export class LoadingSpinnerComponent implements OnInit {
+  public load = true;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.loading();
+  }
+
+  loading() {
+    setTimeout(() => { this.load = false; }, 1500);
+  }
+
+}
